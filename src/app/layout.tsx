3f34082de85225247d79ba80af523892686c7480
rajkunwar4 <40rajkunwar@gmail.com>
2024-07-23 00:42:00 +0530

    initial setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -16,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script src="./node_modules/preline/dist/preline.js" />
       <body className={inter.className}>{children}</body>
     </html>
   );
